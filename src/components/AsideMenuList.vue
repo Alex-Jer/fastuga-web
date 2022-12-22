a817a0a52cb2817bdcbc55b8444f768dfad50b95
Alexandre <alexandrejeronimo2001@gmail.com>
2022-12-22 16:38:19 +0000

    Update AsideMenuList.vue

--- src/components/AsideMenuList.vue
+++ src/components/AsideMenuList.vue
@@ -60,7 +60,9 @@ const userStore = useUserStore()
       <router-link to="/orders">
         <div
           class="flex pt-4 text-gray-300 cursor-pointer hover:text-white dark:hover:text-white"
-          :class="$route.name === 'orders' ? 'dark:text-white font-bold' : 'dark:text-slate-300'"
+          :class="
+            $route.name === 'orders' || $route.name === 'order' ? 'dark:text-white font-bold' : 'dark:text-slate-300'
+          "
         >
           <BaseIcon :path="mdiClipboardTextClockOutline" class="pt-1" w="w-16" :size="18" />
           <span class="pb-1">Orders {{ userStore.user?.type === 'C' ? 'History' : '' }}</span>
