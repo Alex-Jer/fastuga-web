279da6ae2dc334155f3f8550fe394dc13bf80406
Alexandre <alexandrejeronimo2001@gmail.com>
2022-12-22 13:03:25 +0000

    empty cart validation

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { mdiCartOutline, mdiCreditCardCheck } from '@mdi/js'
 import { ref } from 'vue'
+import { useToast } from 'vue-toastification'
 import BaseButton from '@/components/BaseButton.vue'
 import CartTable from '@/components/cart/CartTable.vue'
 import PaymentModal from '@/components/cart/PaymentModal.vue'
@@ -8,18 +9,18 @@ import SectionMain from '@/components/SectionMain.vue'
 import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
 import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
 import { useCartStore } from '@/stores/cart'
-// import { useUserStore } from '@/stores/user'
 
-// const userStore = useUserStore()
 const cartStore = useCartStore()
+const toast = useToast()
 
 const showDetailsModal = ref(false)
 
-const checkout = () => {
+const validate = () => {
+  if (cartStore.items.length === 0) {
+    toast.error('Your cart is empty')
+    return
+  }
   showDetailsModal.value = true
-  // console.log(userStore.user.customer.default_payment_type)
-  // console.log(userStore.user.customer.default_payment_reference)
-  // cartStore.placeOrder()
 }
 </script>
 
@@ -28,7 +29,14 @@ const checkout = () => {
     <PaymentModal v-model="showDetailsModal" title="Payment Details" />
     <SectionMain>
       <SectionTitleLineWithButton :icon="mdiCartOutline" title="Cart" main>
-        <BaseButton color="info" label="Checkout" :icon="mdiCreditCardCheck" @click="checkout" rounded-full />
+        <BaseButton
+          color="info"
+          label="Checkout"
+          :icon="mdiCreditCardCheck"
+          @click="validate"
+          :disabled="cartStore.items.length === 0"
+          rounded-full
+        />
       </SectionTitleLineWithButton>
 
       <cart-table :products="cartStore.items" />
